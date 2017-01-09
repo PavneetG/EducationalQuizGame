1d2f4f728e905675abb91e149e0338acd738ee04
janujang <janujan.g@hotmail.com>
2017-01-09 12:02:37 -0500

    Player, Stats and Quiz Classes

---
+++ src/Player.java
@@ -0,0 +1,120 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**Name: Pavneet Gill
+	 * Date: December 2016
+	 * Description: This program allow the creation of a plauer 
+	 */
+public class Player {
+
+	private String name; 
+	
+	private String userName; 
+	
+	private String password; 
+	
+	private Statistics stats; 
+	
+	private ImageIcon accountpic;
+	
+	public Player() {
+		// TODO Auto-generated constructor stub
+		this.name = ""; 
+		this.userName = ""; 
+		this.stats = null;
+		this.accountpic = null; 
+		this.password = ""; 
+	}
+
+	//Overloads Constructor initializes the customer data
+	public Player(String name, String userName, String password)
+	{
+			this.name= name; 
+			this.userName = userName; 
+			this.password = password; 
+	}
+	
+	public Player (String info)
+	{
+		String word [];
+		word = info.split(",");
+		this.name = word[0];
+		this.userName = word[1];
+		this.password = word[2];
+	}
+	
+	public void updatePlayerData (String info)
+	{
+		String word [];
+		word = info.split(",");
+		this.name = word[0];
+		this.userName = word[1];
+		this.password = word[2];
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getUserName() 
+	{
+		return userName;
+	}
+
+	public void setUserName(String userName) 
+	{
+		this.userName = userName;
+	}
+
+	public Statistics getStats() 
+	{
+		return stats;
+	}
+
+	public void setAccountpic(ImageIcon accountpic) 
+	{
+		this.accountpic = accountpic;
+	}
+
+	public String getPassword ()
+	{
+	     return password; 
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//self testing
+				//-----------------------------------------------------------------------------------
+				String playerInfo = ""; //creating variable for playerInfo 
+
+
+				playerInfo = JOptionPane.showInputDialog(null, "Enter:"+ "<name>,<userName>,<password>");
+
+				//process Record
+				Player playerRecord = new Player(playerInfo); //creating a Customer constructor 
+
+				System.out.println(playerInfo); 
+				
+				System.out.println("Name: "+ playerRecord.getName()); 
+				
+				System.out.println("UserName: " + playerRecord.getUserName());
+				
+				System.out.println("Password: " + playerRecord.getPassword()); 
+	}
+
+}
---
+++ src/Statistics.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+
+/**
+ * @author Pavneet Gill 
+ * Date: December 28 2016
+ * Description: This class keeps track of the player stats 
+ */
+public class Statistics {
+
+	//creating private variables 
+	private int numQuizzes; 
+	
+	private int numCorrect; 
+	
+	private int numTotal; 
+	
+	private double overallPercentage; 
+	
+	private double averageTime; 
+	
+	private String [] quizRecords; 
+	
+	private String quizName; 
+
+	public Statistics() {
+		// TODO Auto-generated constructor stub
+	}
+
+	//getter and setter methods 
+	public int getNumQuizzes() 
+	{
+		return numQuizzes;
+	}
+	
+	public void setNumQuizzes(int numQuizzes) 
+	{
+		this.numQuizzes = numQuizzes;
+	}
+
+	public int getNumCorrect() 
+	{
+		return numCorrect;
+	}
+
+	public void setNumCorrect(int numCorrect) {
+		this.numCorrect = numCorrect;
+	}
+	
+	public int getNumTotal() {
+		return numTotal;
+	}
+	
+	public void setNumTotal(int numTotal) {
+		this.numTotal = numTotal;
+	}
+	
+	public double getOverallPercentage() {
+		return overallPercentage;
+	}
+
+	public void setOverallPercentage(double overallPercentage) {
+		this.overallPercentage = overallPercentage;
+	}
+
+	public double getAverageTime() {
+		return averageTime;
+	}
+
+	public void setAverageTime(double averageTime) {
+		this.averageTime = averageTime;
+	}
+
+	public String getQuizName() {
+		return quizName;
+	}
+
+
+	public void setQuizName(String quizName) 
+	{
+		this.quizName = quizName;
+	}
+
+	public String[] getQuizRecords() 
+	{
+		return quizRecords;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
