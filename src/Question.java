b64f382cd61c1fb35fd9102b63c4aaa48f2f3c02
thelilyliu <lilyxcliu@gmail.com>
2017-01-09 10:42:11 -0500

    Complete Question class

--- src/Question.java
+++ src/Question.java
@@ -1,9 +1,49 @@
-//this is the question class
+/*
+ * Author: Lily Liu
+ * Date: Monday, January 9, 2016
+ * Description: Generic Question class.
+ */
+
 public class Question {
+	
+	private String question;
+	private double time;
+	
+	public Question(String q) {
+		question = q;
+		time = 0;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	/*
+	 * ==============================
+	 * Getters
+	 * ==============================
+	 */
+	
+	public String getQuestion() {
+		return question;
+	}
+	
+	public double getTime() {
+		return time;
+	}
+	
+	/*
+	 * ==============================
+	 * Setters
+	 * ==============================
+	 */
+	
+	public void setQuestion(String q) {
+		question = q;
+	}
+	
+	public void setTime(double t) {
+		time = t;
 	}
 
 }
