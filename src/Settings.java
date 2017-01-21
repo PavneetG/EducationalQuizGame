b9a059a8622ae4deaea3e9ecd67571eadf25ce72
KevinSub <kevinsubhash@gmail.com>
2017-01-20 21:54:30 -0500

    Commented Settings

--- src/Settings.java
+++ src/Settings.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-
+/*
+ * Author: Kevin Subhash
+ */
 public class Settings extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
@@ -24,8 +26,10 @@ public class Settings extends JFrame implements ActionListener {
 		accounts.loadFile("Players.txt");
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(null);
-
+		getContentPane().setLayout(null);
+		/*
+		 * Creted JButtons with Names
+		 */
 		btnChange1 = new JButton("Change Picture");
 		btnChange2 = new JButton("Change Name");
 		btnChange3 = new JButton("Change UserName");
@@ -37,7 +41,9 @@ public class Settings extends JFrame implements ActionListener {
 		btnCancel = new JButton("Cancel");
 		btnVerifyLogin = new JButton("Verify Login");
 		btnBack = new JButton("Back");
-			
+		/*
+		 * Added fonts to JButtons
+		 */
 		btnChange1.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		btnChange2.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		btnChange3.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
@@ -49,8 +55,9 @@ public class Settings extends JFrame implements ActionListener {
 		btnCancel.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		btnVerifyLogin.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
 		btnBack.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
-		
-		
+		/*
+		 * SetBounds for each JButton
+		 */
 		btnChange1.setBounds(10, 251, 184, 38);
 		btnChange2.setBounds(10, 300, 184, 38);
 		btnChange3.setBounds(10, 349, 184, 38);
@@ -62,188 +69,182 @@ public class Settings extends JFrame implements ActionListener {
 		btnCancel.setBounds(10, 398, 184, 38);
 		btnVerifyLogin.setBounds(76, 269, 184, 38);
 		btnBack.setBounds(10, 11, 89, 23);
-		
-		//getContentPane().add(btnBack);
-		btnBack.setVisible(false);
-		btnBack.addActionListener(this);
-		
-		
-		getContentPane().add(btnCancel);
-		btnCancel.setVisible(false);
-		btnCancel.addActionListener(this);
-		
-		
-		getContentPane().add(btnVerifyLogin);
-		btnVerifyLogin.addActionListener(this);
-		
-		
+		/*
+		 * Added frame for each JButton
+		 */
 		getContentPane().add(btnChange1);
-		btnChange1.setVisible(false);
-		btnChange1.addActionListener(this);
-
-		
 		getContentPane().add(btnChange2);
-		btnChange2.addActionListener(this);
-		btnChange2.setVisible(false);
-
 		getContentPane().add(btnChange3);
-		btnChange3.addActionListener(this);
-		btnChange3.setVisible(false);
-		
 		getContentPane().add(btnChange4);
-		btnChange4.addActionListener(this);
-		btnChange4.setVisible(false);
-
 		getContentPane().add(btnConfirm);
-		btnConfirm.addActionListener(this);
-		btnConfirm.setVisible(false);
-
 		getContentPane().add(btnConfirm2);
-		btnConfirm2.addActionListener(this);
-		btnConfirm2.setVisible(false);
-
 		getContentPane().add(btnConfirm3);
-		btnConfirm3.addActionListener(this);
-		btnConfirm3.setVisible(false);
-		
 		getContentPane().add(btnConfirm4);
-		btnConfirm4.addActionListener(this);
+		getContentPane().add(btnCancel);
+		getContentPane().add(btnVerifyLogin);
+		getContentPane().add(btnBack);
+		/*
+		 * Set visibility of certain JButtons to false
+		 */
+		btnChange1.setVisible(false);
+		btnChange2.setVisible(false);
+		btnChange3.setVisible(false);
+		btnChange4.setVisible(false);
+		btnConfirm.setVisible(false);
+		btnConfirm2.setVisible(false);
+		btnConfirm3.setVisible(false);
 		btnConfirm4.setVisible(false);
-
-		textField = new JTextField();
-		textField.setBounds(10, 318, 184, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		textField.setVisible(false);
-
-		lblChange1 = new JLabel("Enter New Name");
-		lblChange1.setBounds(10, 300, 153, 14);
-		getContentPane().add(lblChange1);
-		lblChange1.setVisible(false);
-
-		lblChange2 = new JLabel("Enter New UserName");
-		lblChange2.setBounds(10, 300, 153, 14);
-		getContentPane().add(lblChange2);
-		lblChange2.setVisible(false);
-
-		textField2 = new JTextField();
-		textField2.setBounds(10, 269, 184, 20);
-		getContentPane().add(textField2);
-		textField2.setColumns(10);
-		textField2.setVisible(false);
-
-		textField3 = new JTextField();
-		textField3.setColumns(10);
-		textField3.setBounds(10, 318, 184, 20);
-		getContentPane().add(textField3);
-		textField3.setVisible(false);
-
-		lblChange3 = new JLabel("Enter New Password");
-		lblChange3.setBounds(10, 256, 132, 14);
-		getContentPane().add(lblChange3);
-		lblChange3.setVisible(false);
-
-		lblChange4 = new JLabel("Confirm Password");
-		lblChange4.setBounds(10, 300, 121, 14);
-		getContentPane().add(lblChange4);
-		lblChange4.setVisible(false);
-
-		textField4 = new JTextField();
-		textField4.setBounds(10, 318, 184, 20);
-		getContentPane().add(textField4);
-		textField4.setColumns(10);
-		textField4.setVisible(false);
-
-		label = new JLabel("");
-		label.setIcon(new ImageIcon("p1.jpg"));
-		label.setBounds(37, 280, 60, 60);
-		getContentPane().add(label);
-		label.setVisible(false);
-
-		label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon("p4.png"));
-		label_1.setBounds(107, 280, 60, 60);
-		getContentPane().add(label_1);
-		label_1.setVisible(false);
-
-		label_2 = new JLabel("");
-		label_2.setIcon(new ImageIcon("p3.jpg"));
-		label_2.setBounds(177, 280, 60, 60);
-		getContentPane().add(label_2);
-		label_2.setVisible(false);
-
-		label_3 = new JLabel("");
-		label_3.setIcon(new ImageIcon("p2.png"));
-		label_3.setBounds(246, 280, 60, 60);
-		getContentPane().add(label_3);
-		label_3.setVisible(false);
-
-		radioButton = new JRadioButton("");
+		btnCancel.setVisible(false);
+		btnBack.setVisible(false);
+		/*
+		 * Added actionlistener to all JButtons
+		 */
+		btnChange1.addActionListener(this);	
+		btnChange2.addActionListener(this);	
+		btnChange3.addActionListener(this);	
+		btnChange4.addActionListener(this);
+		btnConfirm.addActionListener(this);
+		btnConfirm2.addActionListener(this);
+		btnConfirm3.addActionListener(this);
+		btnConfirm4.addActionListener(this);
+		btnCancel.addActionListener(this);
+		btnVerifyLogin.addActionListener(this);
+		btnBack.addActionListener(this);
+		/*
+		 * Creted JRadioButtons 
+		 */
+		radioButton = new JRadioButton();
+		radioButton_1 = new JRadioButton();
+		radioButton_2 = new JRadioButton();
+		radioButton_3 = new JRadioButton();
+		/*
+		 * SetBounds for each radiobutton
+		 */
 		radioButton.setBounds(56, 250, 28, 23);
-		getContentPane().add(radioButton);
-		radioButton.setVisible(false);
-		radioButton.addActionListener(this);
-
-		radioButton_1 = new JRadioButton("");
 		radioButton_1.setBounds(127, 250, 28, 23);
-		getContentPane().add(radioButton_1);
-		radioButton_1.setVisible(false);
-		radioButton_1.addActionListener(this);
-
-		radioButton_2 = new JRadioButton("");
 		radioButton_2.setBounds(198, 250, 28, 23);
-		getContentPane().add(radioButton_2);
-		radioButton_2.setVisible(false);
-		radioButton_2.addActionListener(this);
-
-		radioButton_3 = new JRadioButton("");
 		radioButton_3.setBounds(266, 250, 28, 23);
+		/*
+		 * Set frame for each radiobutton
+		 */
+		getContentPane().add(radioButton);
+		getContentPane().add(radioButton_1);
+		getContentPane().add(radioButton_2);
 		getContentPane().add(radioButton_3);
+		/*
+		 * Set visibility of radioButtons to false
+		 */
+		radioButton.setVisible(false);
+		radioButton_1.setVisible(false);
+		radioButton_2.setVisible(false);
 		radioButton_3.setVisible(false);
+		/*
+		 * Added actionlistener to radiobuttons
+		 */
+		radioButton.addActionListener(this);
+		radioButton_1.addActionListener(this);
+		radioButton_2.addActionListener(this);
 		radioButton_3.addActionListener(this);
-
-		lblEmptyBox = new JLabel("Fill in the Field(s).");
-		lblEmptyBox.setBounds(50, 281, 161, 14);
-		getContentPane().add(lblEmptyBox);
-		lblEmptyBox.setVisible(false);
-
-		lblPass = new JLabel("Password DO NOT Match.");
-		lblPass.setBounds(50, 235, 161, 14);
-		getContentPane().add(lblPass);
-		lblPass.setVisible(false);
-
-		
-
-		usernameField = new JTextField();
+		/*
+		 * Creted JTextFields with 10 columns 
+		 */
+		textField = new JTextField(10);
+		textField2 = new JTextField(10);
+		textField3 = new JTextField(10);
+		textField4 = new JTextField(10);
+		usernameField = new JTextField(10);
+		passwordField = new JPasswordField(10);
+		/*
+		 * SetBounds for textfields
+		 */
+		textField.setBounds(10, 318, 184, 20);
+		textField2.setBounds(10, 269, 184, 20);
+		textField3.setBounds(10, 318, 184, 20);
+		textField4.setBounds(10, 318, 184, 20);
 		usernameField.setBounds(91, 165, 153, 20);
-		getContentPane().add(usernameField);
-		usernameField.setColumns(10);
-
-		passwordField = new JPasswordField();
-		passwordField.setColumns(10);
 		passwordField.setBounds(91, 220, 153, 20);
+		/*
+		 * Added textfield to frame
+		 */
+		getContentPane().add(textField);
+		getContentPane().add(textField2);
+		getContentPane().add(textField3);
+		getContentPane().add(textField4);
+		getContentPane().add(usernameField);
 		getContentPane().add(passwordField);
-
+		/*
+		 * Set visibility to certain textfields to false
+		 */
+		textField.setVisible(false);
+		textField2.setVisible(false);
+		textField3.setVisible(false);
+		textField4.setVisible(false);
+		/*
+		 * Creted JLabels with text
+		 */
+		lblChange1 = new JLabel("Enter New Name");
+		lblChange2 = new JLabel("Enter New UserName");
+		lblChange3 = new JLabel("Enter New Password");
+		lblChange4 = new JLabel("Confirm Password");
+		label = new JLabel(new ImageIcon("p1.jpg"));
+		label_1 = new JLabel(new ImageIcon("p4.png"));
+		label_2 = new JLabel(new ImageIcon("p3.jpg"));
+		label_3 = new JLabel(new ImageIcon("p2.png"));
+		lblEmptyBox = new JLabel("Fill in the Field(s).");
 		lblUserName = new JLabel("User Name");
-		lblUserName.setBounds(91, 149, 83, 14);
-		getContentPane().add(lblUserName);
-
 		lblPassword = new JLabel("Password");
-		lblPassword.setBounds(91, 205, 83, 14);
-		getContentPane().add(lblPassword);
-
-		
-
+		lblPass = new JLabel("Password DO NOT Match.");
 		lblTaken = new JLabel("UserName Taken Already.");
+		/*
+		 * SetBounds for labels
+		 */
+		lblChange1.setBounds(10, 300, 153, 14);
+		lblChange2.setBounds(10, 300, 153, 14);
+		lblChange3.setBounds(10, 256, 132, 14);
+		lblChange4.setBounds(10, 300, 121, 14);
+		label.setBounds(37, 280, 60, 60);
+		label_1.setBounds(107, 280, 60, 60);
+		label_2.setBounds(177, 280, 60, 60);
+		label_3.setBounds(246, 280, 60, 60);
+		lblEmptyBox.setBounds(50, 281, 161, 14);
+		lblPass.setBounds(50, 235, 161, 14);
+		lblUserName.setBounds(91, 149, 83, 14);
+		lblPassword.setBounds(91, 205, 83, 14);
 		lblTaken.setBounds(37, 275, 189, 14);
+		/*
+		 * Added labels to frame
+		 */
+		getContentPane().add(lblChange1);
+		getContentPane().add(lblChange2);
+		getContentPane().add(lblChange3);
+		getContentPane().add(lblChange4);
+		getContentPane().add(label);
+		getContentPane().add(label_1);
+		getContentPane().add(label_2);
+		getContentPane().add(label_3);
+		getContentPane().add(lblEmptyBox);
+		getContentPane().add(lblPass);
+		getContentPane().add(lblUserName);
+		getContentPane().add(lblPassword);
 		getContentPane().add(lblTaken);
+		/*
+		 * set visibility for certain labels to false
+		 */
+		lblChange1.setVisible(false);
+		lblChange2.setVisible(false);
+		lblChange3.setVisible(false);	
+		lblChange4.setVisible(false);		
+		label.setVisible(false);	
+		label_1.setVisible(false);	
+		label_2.setVisible(false);
+		label_3.setVisible(false);
+		lblEmptyBox.setVisible(false);
+		lblPass.setVisible(false);
 		lblTaken.setVisible(false);
 
-		
-
-		setSize(350,500);
+		setSize(350,500); // set size of window
 		setVisible(true);
-		setResizable(false);
+		setResizable(false); // cannot resize window
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -254,8 +255,8 @@ public class Settings extends JFrame implements ActionListener {
 				} catch (IOException e1) {
 				}
 			}
-			new HomeMenuGUI();
-			dispose();
+			new HomeMenuGUI(); // calls HomeMenuGUI
+			dispose(); // closes settings gui
 		}
 		if(e.getSource() == btnVerifyLogin){
 			if(accounts.checkLogin(usernameField.getText(), passwordField.getText())){
@@ -393,12 +394,11 @@ public class Settings extends JFrame implements ActionListener {
 			radioButton.setSelected(false);
 		}
 		if (e.getSource() == btnConfirm) {
-			
+
 			/*
 			 * When a specific radiobutton is selcted, it will
 			 * set the picName to the picture's name
 			 */
-
 			if(radioButton.isSelected()){
 				picName = "p1.jpg";
 			}
@@ -433,8 +433,6 @@ public class Settings extends JFrame implements ActionListener {
 			label_2.setVisible(false);
 			label_3.setVisible(false);
 			btnConfirm.setVisible(false);
-
-
 		}
 		if (e.getSource() == btnConfirm2) {
 			if(textField.getText().equals("")){
@@ -456,7 +454,6 @@ public class Settings extends JFrame implements ActionListener {
 				lblChange1.setVisible(false);
 				btnConfirm2.setVisible(false);
 			}
-
 		}
 		if (e.getSource() == btnConfirm3) { 
 			if(textField4.getText().equals("")){ // checking if textfield is empty
@@ -468,7 +465,6 @@ public class Settings extends JFrame implements ActionListener {
 				lblTaken.setVisible(true); // making label visible true
 			}
 			else{
-
 				userName = textField4.getText(); // userName hold text in textField4
 				/*
 				 * Setting visibility of buttons, labels, and textfields 
@@ -498,7 +494,6 @@ public class Settings extends JFrame implements ActionListener {
 				lblPass.setVisible(true); // setting lblPass visible to true
 			}
 			else{
-
 				password = textField2.getText(); // password hold the text in textField2
 				/*
 				 * Setting visibility of buttons, labels, and textfields 
@@ -518,7 +513,6 @@ public class Settings extends JFrame implements ActionListener {
 				lblPass.setVisible(false);
 				btnConfirm4.setVisible(false);
 			}
-
 		}
 		if (e.getSource() == btnCancel) {
 			/*
@@ -555,10 +549,7 @@ public class Settings extends JFrame implements ActionListener {
 			lblEmptyBox.setVisible(false);
 			lblPass.setVisible(false);
 			btnConfirm4.setVisible(false);
-
-
 		}
-
 		newInfo = new Player(name, userName, password, picName);
 	}
 
