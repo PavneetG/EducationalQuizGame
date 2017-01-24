33e2e169017841a2481da6cd91184bfe3c47c87f
KevinSub <kevinsubhash@gmail.com>
2017-01-24 16:16:23 -0500

    Commented statistics gui

--- src/StatisticsGUI.java
+++ src/StatisticsGUI.java
@@ -15,63 +15,53 @@ import java.awt.SystemColor;
 import javax.swing.table.DefaultTableModel;
 /*
  * Author: Kevin Subhash
+ * Date: January 2017
+ * Description: This class is the GUI for the the player Statistics. It displays
+ * the player's quizzes attempted, total questions attempted, correct answers,
+ * overal percentage, and average time.
  */
 public class StatisticsGUI extends JFrame {
 
-	private JPanel contentPane;
-	private JLabel lblPicture, lblPlayerName; 
+	private JLabel lblPicture, lblPlayerName; //creating JLabels and JTable
 	private JTable table;
 
 	public static void main(String[] args) throws IOException {
 		StatisticsGUI statistics = new StatisticsGUI();
 	}
 
-	/**
-	 * Create the frame.
-	 * @throws IOException 
-	 */
 	public StatisticsGUI() throws IOException {
 		
-		Data.accounts.loadFile("Players.txt");
+		Data.accounts.loadFile("Players.txt"); // loads Players.txt
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		getContentPane().setLayout(null); // setting layout to null
 		
-		String[] columns = {"Quizzes", "Total Questions", "Correct Answers", "Overall %", "Average Time"};
+		String[] columns = {"Quizzes", "Total Questions", "Correct Answers", "Overall %", "Average Time"}; // creating columns for JTable
 		Object [][] data = {{Data.accounts.getStats(Data.userName).getNumQuizzes(), Data.accounts.getStats(Data.userName).getNumTotal(), Data.accounts.getStats(Data.userName).getNumCorrect(),
-			Data.accounts.getStats(Data.userName).getOverallPercentage(), Data.accounts.getStats(Data.userName).getAverageTime()}};
+			Data.accounts.getStats(Data.userName).getOverallPercentage(), Data.accounts.getStats(Data.userName).getAverageTime()}}; // Creating and declaring 2D Arrays which contain data for the JTable
 		
-		table = new JTable(data, columns);
-		table.getColumnModel().getColumn(1).setPreferredWidth(92);
+		table = new JTable(data, columns); // created JTable with data and columns
+		table.getColumnModel().getColumn(1).setPreferredWidth(92); // setting the width of the column names
 		table.getColumnModel().getColumn(2).setPreferredWidth(98);
 		table.getColumnModel().getColumn(4).setPreferredWidth(87);
-		table.setBounds(38, 289, 250, 37);
+		table.setBounds(38, 289, 250, 37); // locating the JTable
 		table.setPreferredScrollableViewportSize(new Dimension(500, 50));
 		table.setFillsViewportHeight(true);
-		getContentPane().add(table);
+		getContentPane().add(table); // adding JTable to frame
 		
-		lblPicture = new JLabel(new ImageIcon(Data.accounts.getPic(Data.userName)));
-		lblPlayerName = new JLabel("<html>" + Data.userName + "'s Statistics" + "<html>");
+		lblPicture = new JLabel(new ImageIcon(Data.accounts.getPic(Data.userName))); // using the player's picture for JLabel
+		lblPlayerName = new JLabel("<html>" + Data.userName + "'s Statistics" + "<html>"); // JLabel to state whos statistics it is
 		
-		lblPlayerName.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblPlayerName.setFont(new Font("Tahoma", Font.PLAIN, 20)); // settings font for lblPlayerName
 		
-		lblPicture.setBounds(148, 99, 46, 14);
-		lblPlayerName.setBounds(104, 151, 230, 25);
+		lblPicture.setBounds(148, 99, 46, 14); // locating lblPicture
+		lblPlayerName.setBounds(104, 151, 230, 25); // locating lblPlayerName
 		
-		getContentPane().add(lblPicture);
-		getContentPane().add(lblPlayerName);
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		getContentPane().add(lblPicture); // adds lblPicture to frame
+		getContentPane().add(lblPlayerName); // adds lblPlayerName to frame
 		
 		setSize(350,500); // set size of window
-		setVisible(true);
+		setVisible(true); // setting the visibility to true
 		setResizable(false); // cannot resize window
 	}
 }
